0ab61ddfc9bf93279599a6bbf85c88f8d9658381
ceosss <sswarajsamant@gmail.com>
2020-06-25 22:20:39 +0530

    Completed Experience Page

--- src/components/Experience/Experience.component.jsx
+++ src/components/Experience/Experience.component.jsx
@@ -1,25 +1,85 @@
 import React from "react";
 
+import Draggable from "react-draggable";
+
+import "xp.css/dist/XP.css";
 import "./Experience.styles.css";
 
-const Experience = () => {
+const Experience = ({ history }) => {
+  const sendHome = () => {
+    history.push("/");
+  };
   return (
-    <div className="experience">
-      <h1>Experience</h1>
-      {/* &#x2022; */}
+    <Draggable handle="div.title-bar">
+      <div className="experience">
+        <div
+          className="title-bar"
+          style={{
+            width: 75 + "%",
+            paddingTop: 10 + "px",
+            paddingBottom: 10 + "px",
+            paddingLeft: 10 + "px",
+          }}
+        >
+          <div className="title-bar-text title">Experience.exe</div>
+          <div className="title-bar-controls">
+            <button
+              aria-label="Close"
+              className="close"
+              onClick={sendHome}
+            ></button>
+          </div>
+        </div>
+        <ul
+          class="tree-view"
+          style={{ border: "none", padding: "20px", fontSize: "17px" }}
+        >
+          <li>
+            <strong>TYPE</strong>
+          </li>
+          <li>
+            <details closed>
+              <summary>
+                {" "}
+                <strong>Internship</strong>
+              </summary>
+              <ul>
+                <div className="content">
+                  <div className="exp-content">
+                    <h2>WTF - Digital Waiter </h2>
 
-      <h2> &#x2022; WTF-Digital Waiter </h2>
-      <div className="vertical">
-        <span className="tenure">June 2020 - Present</span>
+                    <span className="tenure">June 2020 - Present</span>
 
-        <h3>SDE Intern </h3>
-        <span>
-          <p> - Designed and Developed Database Models.</p>
-          <p> - Made RESTful API's for large production Applications.</p>
-          <p> - Documented Architecture for all RESTful API's using Swagger.</p>
-        </span>
+                    <h4>SDE Intern </h4>
+                    <span>
+                      <p> - Designed and Developed Database Models.</p>
+                      <p>
+                        {" "}
+                        - Made RESTful API's for large production Applications.
+                      </p>
+                      <p>
+                        {" "}
+                        - Documented Architecture for all RESTful API's using
+                        Swagger.
+                      </p>
+                    </span>
+                  </div>
+                  <div className="exp-logo">
+                    <img src={require("./wtf.menu.jpg")} alt="logo" />
+                  </div>
+                </div>
+              </ul>
+            </details>
+            <details closed>
+              <summary>
+                {" "}
+                <strong>Jobs</strong>
+              </summary>
+            </details>
+          </li>
+        </ul>
       </div>
-    </div>
+    </Draggable>
   );
 };
 
